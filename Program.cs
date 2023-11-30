f0c81709280fe922342cfd9f8380dfb5f0ea5eb8
isabelnecessary <131869667+isabelnecessary@users.noreply.github.com>
2023-11-30 15:41:12 +0000

    added return statement to howManyPlayers

--- Program.cs
+++ Program.cs
@@ -13,30 +13,43 @@ b) Gimel  means “gantz” or “everything.” The player gets everything in t
 c) Hey means “halb” or “half.” The player gets half of the pot. (If there is an odd number of pieces in the pot, the player takes half of the total plus one).
 d) Shin (outside of Israel) means “shtel” or “put in.” */
 
+string readResult = "";
+bool numberOfPlayersIsValidInteger = false;
+
+
 int numberOfPlayers = 0;
 string[] playerNames = new string[numberOfPlayers]; 
 string[] potentialMaxPlayers = {"Player1, Player2, Player3, Player4"};
 int pot = numberOfPlayers * 5;
 int playerScore = 0;
-Array.Copy(potentialMaxPlayers, playerNames, numberOfPlayers-1); //QUESTION should this be there or elsewhere?
+Array.Copy(potentialMaxPlayers, playerNames, numberOfPlayers); //QUESTION should this be there or elsewhere?
 
 Random random = new Random();
 string[] dreidelSides = {"\u05E0", "\u05D2", "\u05D4", "\u05E9"}; // nun, gimmel, hey, shin
-int dreidelSpinRandom = random.Next(1, 4); // dreidelSides.Count +1);
-string dreidelSpinResult = dreidelSides[dreidelSpinRandom];
+int integerDreidelSpin = random.Next(1, 4); // dreidelSides.Count +1);
+string hebrewLetterDreidelSpin = dreidelSides[integerDreidelSpin];
 
 
 makePlayer();
+// setPlayerOrder();
+gameTurn();
+
+int howManyPlayers()
+{
+    Console.WriteLine("enter the number of players (max 4)");
+    string ?readResult = Console.ReadLine();
+
+    //Int32.TryParse(readResult, out numberOfPlayers);
+    numberOfPlayersIsValidInteger = Int32.TryParse(readResult, out numberOfPlayers);
+
+    return numberOfPlayers;
+}
 
 void makePlayer()
 {
-    Console.WriteLine("enter the number of players (max 4)");
-    string ?readResult = Console.ReadLine(); // TODO make this its own method so I can loop it 
+    howManyPlayers();
 
-    Int32.TryParse(readResult, out numberOfPlayers);
-    bool isValidInteger = Int32.TryParse(readResult, out numberOfPlayers);
-
-    if (isValidInteger) 
+    if (numberOfPlayersIsValidInteger) 
         {
             Console.WriteLine($"You are playing dreidel with {numberOfPlayers-1} {(numberOfPlayers > 2 ? "friends" : "friend")}!");
         } 
@@ -45,12 +58,12 @@ void makePlayer()
             Console.WriteLine($"You entered {readResult}, please enter a valid number between 1 and 4");
         }
 
-    if (numberOfPlayers > 4) Console.WriteLine("Sorry, you need at least 2 and at most 4 players to play dreidel! Enter a new player value");
+    if (numberOfPlayers > 4) Console.WriteLine("Sorry, you need between 2 and 4 people to play dreidel! Enter a new player value");
     
     //TODO add try again functionality
 
     Console.WriteLine("Here are the players: ");
-    foreach (string player in playerNames) Console.WriteLine(player);
+    //foreach (string player in playerNames) Console.WriteLine(player);
 }
 
 void setPlayerOrder()
@@ -58,7 +71,9 @@ void setPlayerOrder()
     Console.WriteLine("let's do a practice spin to see who goes first!");
     for (int i = 0; i < numberOfPlayers; i++) // each player in playerNames
     {   
-        Console.WriteLine($"{playerNames[i]}\t{dreidelSpinRandom}");
+        //TODO need to store everyone's first spins
+        // TODO need to reorganise playerNames based on this!
+        Console.WriteLine($"{playerNames[i]}\t{hebrewLetterDreidelSpin}");
     }
     
     //if dreidelSpinRandom == dreidelSpinRandom 
@@ -72,7 +87,7 @@ void gameTurn()
 {
     foreach (string player in playerNames) {
     // spin the dreidel
-        switch (dreidelSpinResult)
+        switch (hebrewLetterDreidelSpin)
         {
             case "\u05E0":
                 Console.WriteLine("\u05E0"); //nun - use unicode to show hebrew nun
