5b1447a2953fe12434c7455cf6a6eee375342bab
isabelnecessary <131869667+isabelnecessary@users.noreply.github.com>
2023-12-06 14:30:01 +0000

    rescoped dreidel spin variables to be local

--- Program.cs
+++ Program.cs
@@ -13,64 +13,50 @@ b) Gimel  means “gantz” or “everything.” The player gets everything in t
 c) Hey means “halb” or “half.” The player gets half of the pot. (If there is an odd number of pieces in the pot, the player takes half of the total plus one).
 d) Shin (outside of Israel) means “shtel” or “put in.” */
 
-string readResult = "";
+string? readResult = "";
 bool numberOfPlayersIsValidInteger = false;
 
-
-int numberOfPlayers = 0;
-string[] playerNames = new string[numberOfPlayers]; 
-int[] playerScores = new int[numberOfPlayers];
-string[] potentialMaxPlayers = {"Player1", "Player2", "Player3", "Player4"};
-
-
-int pot = numberOfPlayers * 5;
 int playerScore = 0;
 
-Random random = new Random();
-string[] dreidelSides = {"\u05E0", "\u05D2", "\u05D4", "\u05E9"}; // nun, gimmel, hey, shin
-int integerDreidelSpin = random.Next(1, 4); // dreidelSides.Count +1);
-string hebrewLetterDreidelSpin = dreidelSides[integerDreidelSpin];
+string[] potentialMaxPlayers = {"Player1", "Player2", "Player3", "Player4"};
+string[] playerNames = new string[potentialMaxPlayers.Length]; 
+int[] playerScores = new int[potentialMaxPlayers.Length];
 
+int numberOfPlayers = howManyPlayers();
+int pot = numberOfPlayers * 5;
 
 makePlayer();
 // setPlayerOrder();
-gameTurn();
+gameTurn(playerNames);
+
 
 int howManyPlayers()
 {
     Console.WriteLine("enter the number of players (max 4)");
-    string ?readResult = Console.ReadLine();
+    string? readResult = Console.ReadLine();
 
-    //Int32.TryParse(readResult, out numberOfPlayers);
-    numberOfPlayersIsValidInteger = Int32.TryParse(readResult, out numberOfPlayers);
+    numberOfPlayersIsValidInteger = int.TryParse(readResult, out int numberOfPlayers);
+
+    if (numberOfPlayersIsValidInteger && numberOfPlayers > 0 && numberOfPlayers <= 4 ) 
+    {
+        
+        Console.WriteLine($"You are playing dreidel with {numberOfPlayers-1} {(numberOfPlayers > 2 || numberOfPlayers == 0 ? "friends" : "friend")}!");
+        //TODO if there's 0 other players   
+    } 
+    else
+    {   
+        Console.WriteLine($"You entered {readResult}, please enter a valid number between 1 and 4");
+        howManyPlayers();
+    }  
 
     return numberOfPlayers;
 }
 
 void makePlayer()
 {
-    howManyPlayers();
-
-    if (numberOfPlayersIsValidInteger) 
-        {
-            Console.WriteLine($"You are playing dreidel with {numberOfPlayers-1} {(numberOfPlayers > 2 ? "friends" : "friend")}!");
-        } 
-    else
-        {
-            Console.WriteLine($"You entered {readResult}, please enter a valid number between 1 and 4");
-        }
-
-    if (numberOfPlayers > 4) 
-        {
-            Console.WriteLine("Sorry, you need between 2 and 4 people to play dreidel! Enter a new player value");
-            howManyPlayers();
-        }
-    else
-        {
-            Array.Copy(potentialMaxPlayers, playerNames, numberOfPlayers-1); 
-            Console.WriteLine("Here are the players: ");
-            foreach (string player in playerNames) Console.WriteLine(player);
-        }
+    //Array.Copy(potentialMaxPlayers, playerNames, numberOfPlayers-1); 
+    Console.WriteLine("Here are the players: ");
+    foreach (string player in potentialMaxPlayers.Take(numberOfPlayers)) Console.WriteLine(player);
 }
 
 void setPlayerOrder(int numberOfPlayers)
@@ -91,29 +77,40 @@ void setPlayerOrder(int numberOfPlayers)
 }
 
 //TODO different players need different scores
-void gameTurn()
+void gameTurn(string[] playerNames)
 {
+    Random random = new Random();
+    string[] dreidelSides = {"\u05E0", "\u05D2", "\u05D4", "\u05E9"}; // nun, gimmel, hey, shin
+    int integerDreidelSpin = random.Next(1, 4); // dreidelSides.Count +1);
+    string hebrewLetterDreidelSpin = dreidelSides[integerDreidelSpin];
+
     foreach (string player in playerNames) {
     // spin the dreidel
         switch (hebrewLetterDreidelSpin)
         {
             case "\u05E0":
-                Console.WriteLine("\u05E0"); //nun - use unicode to show hebrew nun
-                Console.WriteLine("Do nothing - nisht");
+                Console.WriteLine($"\u05E0 nun"); //nun - use unicode to show hebrew nun
+                Console.WriteLine("Nisht - do nothing");
                 break;
             case "\u05D2":
-                Console.WriteLine("\u05D2"); //gimmel - get the whole pot
+                Console.WriteLine($"\u05D2 gimmel"); //gimmel - get the whole pot
+                Console.WriteLine("Gantz ('everything') - get the whole pot.");
+                
                 playerScore += pot;
                 break;
             case "\u05D4":
-                Console.WriteLine("\u05D4"); // hey - get half the pot, or if pot is odd get half+1
+                Console.WriteLine($"\u05D4 hey"); // hey - get half the pot, or if pot is odd get half+1
                 playerScore = pot % 2 == 0 ? playerScore += pot /2 : playerScore += 1 + pot /2;
+                Console.WriteLine("Halb ('half') - get half the pot.");
                 break;
             case "\u05E9":
-                Console.WriteLine("\u05E9"); // shin - put one in the pot
+                Console.WriteLine($"\u05E9 shin"); // shin - put one in the pot
+                Console.WriteLine("Shtel ('put') - put one of your tokens in the pot.");
                 playerScore --;
                 pot ++;
                 break;
         }
+
+        Console.WriteLine(player, playerScore);
     }
 }
