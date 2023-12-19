b7bf62667db81be48e15c2b5f5178a8ad5f3bd15
isabelnecessary <131869667+isabelnecessary@users.noreply.github.com>
2023-12-19 12:54:52 +0000

    added "win" condition

--- Program.cs
+++ Program.cs
@@ -5,63 +5,68 @@
 Everyone spins and the highest spin goes first
     if two people have the same, keep spinning until a winner
 
-    when a player runs out of tokens, they are out
-    when one player has all the tokens, they have won
-
 a) Nun means “nisht” or “nothing.” The player does nothing.
 b) Gimel  means “gantz” or “everything.” The player gets everything in the pot.
 c) Hey means “halb” or “half.” The player gets half of the pot. (If there is an odd number of pieces in the pot, the player takes half of the total plus one).
-d) Shin (outside of Israel) means “shtel” or “put in.” */
+d) Shin (outside of Israel) means “shtel” or “put in.” 
+
+when a player runs out of tokens, they are out
+when one player has all the tokens, they have won
+*/
 
 bool numberOfPlayersIsValidInteger = false;
 
-string[] potentialMaxPlayers = {"Player1", "Player2", "Player3", "Player4"};
+string[] potentialMaxPlayers = {"Player1", "Player2", "Player3", "Player4", "Player5"};
 string[] playerNames = new string[potentialMaxPlayers.Length]; 
 int[] playerScores = new int[potentialMaxPlayers.Length];
 
-int numberOfPlayers = howManyPlayers();
+int numberOfPlayers = HowManyPlayers();
 int pot = numberOfPlayers * 5;
 
-makePlayer();
+MakePlayer();
 // setPlayerOrder();
-gameTurn(playerNames, playerScores);
+do {
+    GameTurn(playerNames, playerScores);
+} while (numberOfPlayers > 1);
+Console.WriteLine($"Only one player is left - game over, and congratulations to {playerNames[0]} for winning with {playerScores[0]} tokens!");
+//TODO - when players are removed, the corresponding playerScores aren't removed, so I can't print their score as well...
 
 
-int howManyPlayers()
+int HowManyPlayers()
 {
-    Console.WriteLine("enter the number of players (max 4)");
+    Console.WriteLine("enter the number of players (max 5)");
     string? readResult = Console.ReadLine();
 
     numberOfPlayersIsValidInteger = int.TryParse(readResult, out int numberOfPlayers);
 
-    if (numberOfPlayersIsValidInteger && numberOfPlayers > 0 && numberOfPlayers <= 4 ) 
+    if (numberOfPlayersIsValidInteger && numberOfPlayers > 1 && numberOfPlayers <= 5 ) 
     {       
-        Console.WriteLine($"You are playing dreidel with {numberOfPlayers-1} {(numberOfPlayers > 2 || numberOfPlayers == 1 ? "friends" : "friend")}!");
+        Console.WriteLine($"You are playing dreidel with {numberOfPlayers-1} {(numberOfPlayers > 2 ? "friends" : "friend")}!");
     } 
     else
     {   
-        Console.WriteLine($"You entered {readResult}, please enter a valid number between 1 and 4");
-        return howManyPlayers();
+        Console.WriteLine($"You entered {readResult}, please enter a valid number between 2 and 5");
+        return HowManyPlayers();
     }  
 
     return numberOfPlayers;
 }
 
-void makePlayer()
+void MakePlayer()
 {
     Array.Copy(potentialMaxPlayers, playerNames, numberOfPlayers); 
     Console.WriteLine("Here are the players: ");
-    foreach (string player in playerNames /*potentialMaxPlayers.Take(numberOfPlayers)*/ ) Console.WriteLine(player);
+    foreach (string player in playerNames) Console.WriteLine(player);
 }
 
-void setPlayerOrder(int numberOfPlayers)
+void SetPlayerOrder(int numberOfPlayers)
 {   
     string[] initialSpinResults = new string[playerNames.Length];
     
     Console.WriteLine("let's do a practice spin to see who goes first!");
-    for (int i = 0; i < numberOfPlayers; i++) // each player in playerNames
+    for (int i = 0; i < numberOfPlayers; i++)
     {   
-        initialSpinResults[i] = dreidelSpin();
+        initialSpinResults[i] = DreidelSpin();
 
         //TODO need to reorganise playerNames based on this!
         
@@ -76,24 +81,25 @@ void setPlayerOrder(int numberOfPlayers)
 }
 
 //TODO different players need different scores
-void gameTurn(string[] playerNames, int[] playerScores)
+void GameTurn(string[] playerNames, int[] playerScores)
 {
+    Console.WriteLine("\nNew round!");
     for (int i = 0; i < numberOfPlayers; i++) {
     // spin the dreidel
-        switch (dreidelSpin())
+        switch (DreidelSpin())
         {
             case "\u05E0":
-                Console.WriteLine($"\u05E0 nun"); //nun - use unicode to show hebrew nun
+                Console.WriteLine($"{playerNames[i]} has rolled a \u05E0 nun"); //nun - use unicode to show hebrew nun
                 Console.WriteLine("Nisht - do nothing");
                 break;
             case "\u05D2":
-                Console.WriteLine($"\u05D2 gimmel"); //gimmel - get the whole pot
+                Console.WriteLine($"{playerNames[i]} has rolled a \u05D2 gimmel"); //gimmel - get the whole pot
                 Console.WriteLine("Gantz ('everything') - get the whole pot.");
                 playerScores[i] += pot;
                 pot = 0;
                 break;
             case "\u05D4":
-                Console.WriteLine($"\u05D4 hey"); // hey - get half the pot, or if pot is odd get half+1
+                Console.WriteLine($"{playerNames[i]} has rolled a \u05D4 hey"); // hey - get half the pot, or if pot is odd get half+1
                 Console.WriteLine("Halb ('half') - get half the pot.");
                 if (pot % 2 == 0)
                 {
@@ -107,7 +113,7 @@ void gameTurn(string[] playerNames, int[] playerScores)
                 }                
                 break;
             case "\u05E9":
-                Console.WriteLine($"\u05E9 shin"); // shin - put one in the pot
+                Console.WriteLine($"{playerNames[i]} has rolled a \u05E9 shin"); // shin - put one in the pot
                 Console.WriteLine("Shtel ('put') - put one of your tokens in the pot.");
                 if (playerScores[i] > 0)
                 {
@@ -130,21 +136,26 @@ void gameTurn(string[] playerNames, int[] playerScores)
                     numberOfPlayers--;                   
                     int indexToRemove = i; //Array.IndexOf(playerNames, playerNames[i]);
                     playerNames = playerNames.Where((source, index) => index != indexToRemove).ToArray();
+                    playerScores = playerScores.Where((source, index) => index != indexToRemove).ToArray();
                     Console.WriteLine("Here are the current players:");
                     foreach (string player in playerNames) Console.WriteLine(player);
                 }
                 break;
         }
-        Console.WriteLine(playerNames[i], playerScores[i]);
+    }
+    Console.WriteLine("Player scores:");
+    for (int i = 0; i < numberOfPlayers; i++)
+    {
+        Console.WriteLine($"{playerNames[i]} has {playerScores[i]} tokens");
     }
 }
 
-string dreidelSpin() 
+string DreidelSpin() 
 {
     Random random = new Random();
     string[] dreidelSides = {"\u05E0", "\u05D2", "\u05D4", "\u05E9"}; // nun, gimmel, hey, shin
-    int integerDreidelSpin = random.Next(1, 4); // dreidelSides.Count +1);
     
+    int integerDreidelSpin = random.Next(1, 4); // dreidelSides.Count +1);
     string hebrewLetterDreidelSpin = dreidelSides[integerDreidelSpin];
     
     return hebrewLetterDreidelSpin;
